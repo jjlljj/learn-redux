b8ba41e191b3677843ec5321f1330ba587430f15
James Logue <logueja@gmail.com>
2018-01-21 16:39:21 -0700

    Add single view and comments

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router';
+import Photo from './Photo.js'
+import Comments from './Comments.js'
 
 const Single = React.createClass({
   render() {
+
+    const { postId } = this.props.params;
+    const i = this.props.posts.findIndex(post => post.code === postId);
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
+
     return (
       <div className="single-photo">
-        single yo
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} {...this.props} />
       </div>
     )
   }
